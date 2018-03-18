a6b4f26c030de66b67d5e7960960e9724befc8ad
t8303606 <yoav.berkovits.38.talpiot@gmail.com>
2018-03-18 10:34:12 +0200

    the beginig of dream

--- two_turns/filter_colors_2.py
+++ two_turns/filter_colors_2.py
@@ -7,7 +7,6 @@ import chess
 import cv2
 import math
 
-
 BLACK = (0.0, 0.0, 0.0)
 MINIMAL_PLAYER_BOARD_RATIO = 0.2
 PIXELS_FOR_MAIN_COLORS = (200, 200)
@@ -26,26 +25,8 @@ class filter_colors_2:
 
     def __init__(self, im, chess_helper_2):
         self.chess_helper_2 = chess_helper_2
-        self.initialize_colors(im,chess_helper_2.user_starts)
+        self.initialize_colors(im, chess_helper_2.user_starts)
         self.initialize_board()
-
-    def initialize_board(self):
-        self.board = []
-        for i in range(8):
-            self.board.append([])
-            for j in range(8):
-                self.board[i].append(None)
-
-    def update_board(self, last_move):
-        if last_move != None:
-            row1 = ord(last_move[0]) - ord('a')
-            colon1 = int(last_move[1]) - 1
-            row2 = ord(last_move[2]) - ord('a')
-            colon2 = int(last_move[3]) - 1
-            if colon1 < 6:
-                self.board[row1][colon1 + 2] = None
-            if colon1 < 6:
-                self.board[row2][colon2 + 2] = None
 
     def color_dist(self, color1, color2):
         return abs(max(color1) - max(color2) - min(color2) + min(color1))
@@ -53,19 +34,12 @@ class filter_colors_2:
     def cmpT(self, t1, t2):
         return t1[0] == t2[0] and t1[1] == t2[1] and t1[2] == t2[2]
 
-    """
-      gets all 4/3/2 colors in the board.
-      :im initial image, that contains the relevant colors on the board.
-      :return nothing
-      """
-
     def initialize_colors(self, im):
         """
         :param im:
-        :param is_me_white:
         :return black,white,user soldier color, rival soldier color:
         """
-        self.set_prev_im(im)
+        self.prev_im = im
         im_resize = cv2.resize(im, PIXELS_FOR_MAIN_COLORS)
         board_colors = self.get_board_colors(im_resize)
         user_color = self.get_player_color(im_resize, board_colors, USER)
@@ -78,11 +52,11 @@ class filter_colors_2:
         print(main_colors)
         self.main_colors = main_colors
 
-    """
-        gets 2 primary colors from board image.
-        """
-
     def get_board_colors(self, im):
+        """
+        :param im:
+        :return 2 primary colors from board image:
+        """
         # TODO fix this lines
         im_sz = len(im)
         ar = im[(im_sz // 4):(3 * im_sz // 4)]
@@ -106,15 +80,15 @@ class filter_colors_2:
             new_indices.append(indices[0])
         return [codes[i] for i in new_indices]
 
-    """
-        gets player's colors from the board image.
-        """
-
     def get_player_color(self, im, board_colors, player):
-        is_me_white = chess_helper_2.user_starts
-        # ar = np.asarray(im)
+        """
+        :param im:
+        :param board_colors:
+        :param player (user or rival):
+        :return player color in RGB:
+        """
+        user_starts = self.chess_helper_2.user_starts
         ar = im
-        # TODO fix these lines
         ar_sz = len(ar)
         if player == RIVAL:
             ar = ar[:(ar_sz // 4)]
@@ -143,13 +117,13 @@ class filter_colors_2:
                 color_dist_from_black = self.color_dist(pix, board_colors[0])
                 color_dist_from_white = self.color_dist(pix, board_colors[1])
                 if color_dist_from_player < color_dist_from_black and \
-                        color_dist_from_player < color_dist_from_white:
+                                color_dist_from_player < color_dist_from_white:
                     num_of_player_pix += 1
         num_of_pix = len(ar) * len(ar[0])
         rank = num_of_player_pix / num_of_pix
         print(rank)
         if rank < MINIMAL_PLAYER_BOARD_RATIO:
-            if (is_me_white and not player) or (not is_me_white and player):
+            if (user_starts and not player) or (not user_starts and player):
                 player_color = board_colors[0]
             else:
                 player_color = board_colors[1]
@@ -158,7 +132,6 @@ class filter_colors_2:
     def set_colors_nums(self, main_colors):
         self.USER_NUM = 4
         self.RIVAL_NUM = 8
-
         # main colors order: black = 1, white = 2, user = 4, rival = 8
         if self.cmpT(main_colors[2], main_colors[0]):
             self.USER_NUM = BLACK_NUM
@@ -168,67 +141,28 @@ class filter_colors_2:
             self.RIVAL_NUM = BLACK_NUM
         elif self.cmpT(main_colors[3], main_colors[1]):
             self.RIVAL_NUM = WHITE_NUM
-
         self.R2W = WHITE_NUM - self.RIVAL_NUM
         self.R2B = BLACK_NUM - self.RIVAL_NUM
         self.R2U = self.USER_NUM - self.RIVAL_NUM
         self.U2R = self.RIVAL_NUM - self.USER_NUM
         self.B2R = self.RIVAL_NUM - BLACK_NUM
         self.W2R = self.RIVAL_NUM - WHITE_NUM
-
         return
 
     ###########################################################################
 
-    """
-        sets previous image - in rgb format.
+    def get_square_image(self, im, loc):
         """
-
-    def set_prev_im(self, im):
-        self.prev_im = im
-
-    """
-    :im image of square after turn NOT CATALOGUED
-    :square_loc location of square, in uci format
-    :return binary image of relevant differences only (according to
-    player/square color)
-    """
-
-    def get_square_diff(self, im, square_loc, is_source):
-        is_white = self.chess_helper_2.square_color(square_loc) == chess.WHITE
-        row = ord(square_loc[0]) - ord('a')
-        colon = int(square_loc[1]) - 1
-        after_square = cv2.resize(self.get_square_image(im, square_loc, self.chess_helper_2.user_starts), PIXELS_SQUARE)
-        after_square = self.catalogue_colors(after_square, is_white)
-        #maybe_before_square = self.board[row][colon]
-        maybe_before_square = None
-        #TODO fix it (now we are not using the saved board
-        if maybe_before_square == None:
-            before_square = cv2.resize(self.get_square_image(self.prev_im, square_loc, self.chess_helper_2.user_starts), PIXELS_SQUARE)
-            before_square = self.catalogue_colors(before_square, is_white)
-        else:
-            before_square = maybe_before_square
-        self.board[row][colon] = after_square
-        square_diff = self.make_binary_relevant_diff_im(before_square, after_square, is_white, is_source,self.chess_helper_2.curr_player)
-        square_diff_2 = np.array(square_diff)
-        # square_diff_cut = cv2.cvtColor(square_diff_2, cv2.COLOR_GRAY2RGB)
-        # print(square_diff_cut)
-        return square_diff
-
-    """
-    :return an image with 4/3/2 colors only.
-    """
-
-    """
-    returns subimage of a square in the board.
-    """
-
-    def get_square_image(self, im, loc, did_I_start):
+        :param im:
+        :param loc:
+        :return subimage of a square in the board:
+        """
+        user_starts = self.chess_helper_2.user_starts
         locidx = self.chess_helper_2.ucitoidx(loc)
         sq_sz = len(im[0]) // 8
-        sq_sz_y = len(im)//8
+        sq_sz_y = len(im) // 8
         x = locidx[0]
-        if did_I_start:
+        if user_starts:
             y = 8 - locidx[1]
         else:
             y = locidx[1]
@@ -236,49 +170,30 @@ class filter_colors_2:
         sqr_im = im[area[1]:area[3], area[0]:area[2]]
         return sqr_im
 
-    def catalogue_colors(self, im, is_white):
-        main_colors_white = self.main_colors[1:]
-        main_colors_black = [self.main_colors[0]] + self.main_colors[2:]
-        if is_white:
-            cat_im = self.fit_colors(im, main_colors_white, is_white)
-        else:
-            cat_im = self.fit_colors(im, main_colors_black, is_white)
-        return cat_im
-
-    """
-    :return image fit to 4 main colors.
-    """
-
-    def fit_colors(self, im, main_colors, is_white):
+    def fit_colors(self, im):
+        """
+        :param im:
+        :return image fit to 4 main colors:
+        """
         new_im = []
         for rowidx in range(len(im)):
             i = 0
             row = im[rowidx]
             new_im.append([])
             for pix in row:
-                min_dist = self.color_dist(pix, main_colors[0])
-                if is_white:
-                    new_im[rowidx].append(WHITE_NUM)
-                    if not self.cmpT(main_colors[1], main_colors[0]):
-                        dist = self.color_dist(pix, main_colors[1])
-                        if dist < min_dist:
-                            min_dist = dist
-                            new_im[rowidx][i] = self.USER_NUM
-                    if not self.cmpT(main_colors[2], main_colors[0]):
-                        dist = self.color_dist(pix, main_colors[2])
-                        if dist < min_dist:
-                            new_im[rowidx][i] = self.RIVAL_NUM
-                else:
-                    new_im[rowidx].append(BLACK_NUM)
-                    if not self.cmpT(main_colors[1], main_colors[0]):
-                        dist = self.color_dist(pix, main_colors[1])
-                        if dist < min_dist:
-                            min_dist = dist
-                            new_im[rowidx][i] = self.USER_NUM
-                    if not self.cmpT(main_colors[2], main_colors[0]):
-                        dist = self.color_dist(pix, main_colors[2])
-                        if dist < min_dist:
-                            new_im[rowidx][i] = self.RIVAL_NUM
+                min_dist = self.color_dist(pix, self.main_colors[0])
+                new_im[rowidx].append(BLACK_NUM)
+                dist = self.color_dist(pix, self.main_colors[1])
+                if dist < min_dist:
+                    min_dist = dist
+                    new_im[rowidx][i] = WHITE_NUM
+                dist = self.color_dist(pix, self.main_colors[2])
+                if dist < min_dist:
+                    min_dist = dist
+                    new_im[rowidx][i] = self.USER_NUM
+                dist = self.color_dist(pix, self.main_colors[3])
+                if dist < min_dist:
+                    new_im[rowidx][i] = self.RIVAL_NUM
                 i += 1
         return new_im
 
@@ -291,7 +206,7 @@ class filter_colors_2:
             else:
                 RC.append(self.R2B)
             if self.chess_helper_2.piece_color(square):
-                #TODO correct chess_helper - change the colors of pieces to User and Rival
+                # TODO correct chess_helper - change the colors of pieces to User and Rival
                 RC.append(self.R2U)
         else:
             if is_white:
@@ -312,3 +227,25 @@ class filter_colors_2:
                 else:
                     binary_im[rowidx].append(0)
         return binary_im
+
+    def get_square_diff(self, im, square_loc, is_source):
+        """
+        :param im:
+        :param square_loc:
+        :param is_source:
+        :return binary image of relevant changes only (according alot of parameters):
+        """
+        row = ord(square_loc[0]) - ord('a')
+        colon = int(square_loc[1]) - 1
+        after_square = cv2.resize(self.get_square_image(im, square_loc, self.chess_helper_2.user_starts), PIXELS_SQUARE)
+        after_square = self.fit_colors(after_square)
+        maybe_before_square = None
+        if maybe_before_square == None:
+            before_square = cv2.resize(self.get_square_image(self.prev_im, square_loc, self.chess_helper_2.user_starts),
+                                       PIXELS_SQUARE)
+            before_square = self.fit_colors(before_square)
+        else:
+            before_square = maybe_before_square
+        self.board[row][colon] = after_square
+        square_diff = self.make_binary_relevant_diff_im(before_square, after_square, square_loc, is_source)
+        return square_diff
