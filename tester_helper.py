1bd27007f2bdb627bcf4b5e819f3eb2794a5564b
t8291043 <carmelsatt.38.talpiot@gmail.com>
2018-03-21 14:07:53 +0200

    include rgb saves

--- tester_helper.py
+++ tester_helper.py
@@ -2,15 +2,22 @@ import cv2
 import os
 import errno
 import numpy as np
+import scipy.misc
 
 RESULTS_DIR = 'super_tester_results'
-ROWS_NUM = 8
+ROWS_NUM = 9
+COL_NUM = 8
 
 
 
-def save(img, place, move_num, angle_idx, desc = ''):
+def save_bw(img, place, move_num, angle_idx, desc =''):
     cv2.imwrite(RESULTS_DIR + '\\' +'by_move' + '\\' + 'move_num_' + str(move_num) + '\\' + 'angle_num_' + str(angle_idx) + '\\' + place + '_' + desc + '.jpg', img)
     cv2.imwrite(RESULTS_DIR + '\\' +'by_square' + '\\' + place + '\\' + 'angle_num_' + str(angle_idx) + '\\' + str(move_num) + '_' + desc + '.jpg', img)
+
+def save_colors(img, place, move_num, angle_idx, desc =''):
+    scipy.misc.imsave(RESULTS_DIR + '\\' +'by_move' + '\\' + 'move_num_' + str(move_num) + '\\' + 'angle_num_' + str(angle_idx) + '\\' + place + '_' + desc + '.jpg', img)
+    scipy.misc.imsave(RESULTS_DIR + '\\' +'by_square' + '\\' + place + '\\' + 'angle_num_' + str(angle_idx) + '\\' + str(move_num) + '_' + desc + '.jpg', img)
+
 
 
 def make_dir(dir_name):
@@ -23,7 +30,7 @@ def make_dir(dir_name):
 def make_board_im_helper(squares, square_ims):
     squares_im_lst = []
     for i in range(ROWS_NUM):
-        for j in range(ROWS_NUM):
+        for j in range(COL_NUM):
             square = chr(ord('a')+j)+str(ROWS_NUM-i)
             if square in squares:
                 squares_im_lst.append(square_ims[squares.index(square)])
@@ -33,14 +40,14 @@ def make_board_im_helper(squares, square_ims):
 
 def make_board_im(squares_im_lst, pic_hi, pic_wid):
     #lst of all squares. None for non relevant squares.
-    if len(squares_im_lst) != ROWS_NUM**2:
+    if len(squares_im_lst) != ROWS_NUM*COL_NUM:
         raise Exception('make board im has failed')
     row_num = pic_hi*ROWS_NUM
-    col_num = pic_wid*ROWS_NUM
+    col_num = pic_wid*COL_NUM
     im = np.zeros((row_num,col_num), dtype=np.int).tolist()
     for i in range(ROWS_NUM):
-        for j in range(ROWS_NUM):
-            im_num = i*ROWS_NUM+j
+        for j in range(COL_NUM):
+            im_num = i*COL_NUM+j
             if squares_im_lst[im_num] is not None:
                 for k in range(pic_hi):
                     im[i*pic_hi+k][j*pic_wid:(j+1)*pic_wid] = squares_im_lst[i*8+j][k]
--- two_turns/photos_angle_2.py
+++ two_turns/photos_angle_2.py
@@ -4,11 +4,12 @@ import identify_board
 import board_cut_fixer
 import tester_helper
 import numpy as np
+
 print_and_save = True
 
 
 class photos_angle_2:
-    def __init__(self, hardware1, chess_helper,delay_chess_helper, self_idx):
+    def __init__(self, hardware1, chess_helper, delay_chess_helper, self_idx):
         self.chess_helper = chess_helper
         self.delay_chess_helper = delay_chess_helper
         self.hardware = hardware1
@@ -18,31 +19,33 @@ class photos_angle_2:
 
     def init_colors(self):
 
-        cut_board_im = self.get_new_img(tester_info=(-1,self.idx))
-        self.color_filter = two_turns.filter_colors_2.filter_colors_2(cut_board_im, self.chess_helper, self.delay_chess_helper)
+        cut_board_im = self.get_new_img(tester_info=(-1, self.idx))
+        self.color_filter = two_turns.filter_colors_2.filter_colors_2(cut_board_im, self.chess_helper,
+                                                                      self.delay_chess_helper)
 
     def prep_img(self):
         self.prep_im = self.hardware.get_image(self.idx)
 
     def get_new_img(self, tester_info=None):
-        to_save = bool(tester_info)
+        try:
+            to_save = bool(tester_info)
 
-        new_board_im = self.prep_im
+            new_board_im = self.prep_im
 
+            better_cut_board_im = self.fixer.main(new_board_im)
 
-        better_cut_board_im = self.fixer.main(new_board_im)
+            if to_save:
+                move_num = tester_info[0]
+                angle_idx = tester_info[1]
+                tester_helper.save_bw(better_cut_board_im, 'board', move_num, angle_idx, 'second')
 
-        if to_save:
-            move_num = tester_info[0]
-            angle_idx = tester_info[1]
-            tester_helper.save(better_cut_board_im, 'board', move_num, angle_idx, 'second')
-
-        return better_cut_board_im
+            return better_cut_board_im
+        except:
+            print("get new im failed")
+            raise
 
     def get_square_diff(self, cut_board_im, src, is_source):
         return self.color_filter.get_square_diff(cut_board_im, src, is_source)
-
-
 
     def set_prev_im(self, img):
         self.fixer.set_prev_im(img)
